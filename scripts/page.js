4605505ac8ecba9aabbe57d1627f880776802412
jlisi <jlisi@umich.edu>
2020-11-20 02:09:33 -0500

    Added functionality for the user to be able to select a tile to shoot. Also it checks for if the user hit all the tiles and then it ends the game. Did not take part of the front end for hitting or missing a tile though.

--- scripts/page.js
+++ scripts/page.js
@@ -4,8 +4,9 @@
 let gwhGame;
 
 // Player's Ship Dictionary
-let player1_present_dict = {};
-let AI_present_array = [];
+let player1_ship_dict = {};
+let ai_ship_locations = [];
+let ai_ships_locations_alive = [];
 
 // Players Hit Count
 let player1_hitCount = 8;
@@ -13,7 +14,7 @@ let AI_hitCount = 8;
 
 // Players choices (for example: index -> 0/1)
 let player_choices_dict = {}
-let AI_choices_dict = {}
+let ai_choices_dict = {}
 
 // State "ENUM" for game (No apparent enums in JS)
 const GameState = {
@@ -30,31 +31,8 @@ $(document).ready( function() {
   gwhGame = $('#actualGame');
 });
 
-// Check if two objects are colliding
-function isColliding(o1, o2) {
-  const o1D = { 'left': o1.offset().left,
-        'right': o1.offset().left + o1.width(),
-        'top': o1.offset().top,
-        'bottom': o1.offset().top + o1.height()
-      };
-  const o2D = { 'left': o2.offset().left,
-        'right': o2.offset().left + o2.width(),
-        'top': o2.offset().top,
-        'bottom': o2.offset().top + o2.height()
-      };
-  // Adapted from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-  if (o1D.left < o2D.right &&
-    o1D.right > o2D.left &&
-    o1D.top < o2D.bottom &&
-    o1D.bottom > o2D.top) {
-     // collision detected!
-     return true;
-  }
-  return false;
-}
-
-// Present Class Constructor Function
-class Present {
+// Ship Class Constructor Function
+class Ship {
   constructor(id) {
     this.id = id;
     this.placed = false;
@@ -65,7 +43,7 @@ class Present {
 
 // Function to loop and check if all items has been placed
 function isAllItemsPlaced() {
-  for (const [key, value] of Object.entries(player1_present_dict)) {
+  for (const [key, value] of Object.entries(player1_ship_dict)) {
     if (value.placed === false) {
       return false;
     }
@@ -92,13 +70,13 @@ function leave_menu(){
   $(".title").remove();
   $(".single-player-btn").remove();
 
-  $("#actualGame").append( "<h1 id='place_items_title'>Place your items</h1>" );
+  $("#actualGame").append( "<h1 id='game_title'>Place your items</h1>" );
 
   let timer = 30;
   $( "#actualGame" ).append( "<h1 id='timer'>" + timer + "</h1>" );
   let timer_interval = setInterval(function(){
     timer -= 1;
-    $("#timer").html(timer);
+    $("#timer").text(timer);
 
     if (timer == 0){
       clearInterval(timer_interval)
@@ -117,7 +95,7 @@ function create_boards(){
 
   for (let i = 1; i < 26; i++){
     $("#userBoard").append("<div id='user-tile-" + i + "' class='tile user_tile'></div>");
-    $("#opponentBoard").append("<div id='opponent-tile-" + i + "' class='tile'></div>");
+    $("#opponentBoard").append("<div id='opponent-tile-" + i + "' class='tile shootable'></div>");
   }
 
   $("#actualGame").append("<div class='select-legend'></div>")
@@ -142,19 +120,34 @@ function create_boards(){
       update_item_placed(false, ui, $this);
     },
     out: function(event, ui) {
-      $(this).droppable('option', 'accept', '.present');
+      $(this).droppable('option', 'accept', '.ship');
       $( this ).removeClass("selected");
     }
   });
 
   for (let i = 1; i < 9; i++){
-    $('.select-legend').append("<div class='size_1_item'><img src='style/images/present.png' class='present center' id='ship-" + i + "'></div>");
+    let image = ""
 
-    // Create new present object
-    let presentObject = new Present(i);
+    if (i < 3){
+      image = "snowman.png";
+    }
+    else if (i < 5){
+      image = "reindeer.png";
+    }
+    else if (i < 7){
+      image = "christmas_tree.png";
+    }
+    else {
+      image = "present.png";
+    }
 
-    // Push present object to player_1 dictionary --> [index: presentObject] pairs
-    player1_present_dict[i] = presentObject;
+    $('.select-legend').append("<div class='size_1_item'><img src='style/images/" + image + "' class='ship center' id='ship-" + i + "'></div>");
+
+    // Create new ship object
+    let shipObject = new Ship(i);
+
+    // Push ship object to player_1 dictionary --> [index: shipObject] pairs
+    player1_ship_dict[i] = shipObject;
   }
 
   $(".size_1_item").droppable({
@@ -179,11 +172,12 @@ function create_boards(){
     }
   });
 
-  $(".present").draggable({
+  $(".ship").draggable({
     revert: 'invalid',
     disabled: false
   });
 
+  make_ai_ship_choices();
 }
 
 // If the item has been dropped into the user tile, update "placed" property to true
@@ -198,8 +192,8 @@ function update_item_placed(isOrigin, ui, origin=undefined) {
   // If Item is returned to the origin
   if (isOrigin === true) {
     // Modify the placed property
-    player1_present_dict[draggableId[1]].placed = false;
-    player1_present_dict[draggableId[1]].location = 0;
+    player1_ship_dict[draggableId[1]].placed = false;
+    player1_ship_dict[draggableId[1]].location = 0;
 
   } else {
     // get the id of the origin
@@ -207,13 +201,13 @@ function update_item_placed(isOrigin, ui, origin=undefined) {
     let originId = rawOriginId.split("-");
 
     // Modify the placed property
-    player1_present_dict[draggableId[1]].placed = true;
-    player1_present_dict[draggableId[1]].location = originId[2];
+    player1_ship_dict[draggableId[1]].placed = true;
+    player1_ship_dict[draggableId[1]].location = originId[2];
   }
 }
 
 function end_game(){
-  $("#actualGame").html("<h1 id='place_items_title' style='top: 30%; color: red;'>Game Ended</h1>");
+  $("#actualGame").html("<h1 id='game_title' style='top: 30%; color: red;'>Game Ended</h1>");
   $("#actualGame").append("<button type='button' class='btn btn-primary btn-lg single-player-btn' onclick='menu_button()' style='top: 40%'>Menu</button>");
 }
 
@@ -224,14 +218,14 @@ function start_single_player_game_interaction() {
 
 
   // Change Instruction Text Label
-  $("#place_items_title").text("Start Match");
+  $("#game_title").text("Start Match");
 
   // Disable Draggable Interactions
-  $(".present").draggable('disable');
+  $(".ship").draggable('disable');
 
 
   // Move all child div inside Legend div into each divs inside the grid tiles and remove style attribute for positioning
-  for (const [key, value] of Object.entries(player1_present_dict)) {
+  for (const [key, value] of Object.entries(player1_ship_dict)) {
     $("#ship-" + key).appendTo($("#user-tile-" + value.location));
     $("#ship-" + key).removeAttr('style');
     $("#ship-" + key).css("top", ".3vw");
@@ -251,30 +245,97 @@ function runGame(state) {
     end_game()
     return;
   }
+
   switch (state) {
     case GameState.PLAYERTURN:
       console.log("player turn");
+
       // Player can choose opponent grid
+      let timer = 15;
+      $("#timer").html(timer);
+      let timer_interval = setInterval(function(){
+        timer -= 1;
+        $("#timer").text(timer);
 
+        if (timer == 0){
+          clearInterval(timer_interval);
+          $( ".shootable").unbind( "mouseover" );
+          $( ".shootable").unbind( "mouseleave" );
+          $( ".shootable").unbind( "click" );
+          $( ".shootable").removeClass("highlight");
+        }
+      }, 1000); 
 
-      // Button to Fire
+      $("#game_title").text("Select a square to shoot");
 
+      $( ".shootable" ).bind({
+        mouseover: function() {
+          // highlight the mouseover target
+          $(event.target).addClass("highlight");
+        },
+        mouseleave: function() {
+          $(event.target).removeClass("highlight");
+        },
+        click: function() {
+          $( ".shootable").unbind( "mouseover" );
+          $( ".shootable").unbind( "mouseleave" );
+          $( ".shootable").unbind( "click" );
+          $( ".shootable").removeClass("highlight");
+          clearInterval(timer_interval);
+          $("#timer").text("");
 
-      //Set timer to 15 seconds
+          let target_id = $(event.target).attr("id");
+  
+          target_id = parseInt(target_id.split("-")[2]);
 
-      // End game for testing purpose
-      runGame(GameState.GAMEOVER);
+          if(ai_ships_locations_alive.includes(target_id)){
+            console.log("HIT");
+
+            ai_ships_locations_alive.splice(ai_ships_locations_alive.indexOf(target_id), 1);
+            $(event.target).removeClass("shootable");
+
+            console.log(ai_ships_locations_alive.length);
+
+            // Need to append image of a hit sign to the event.target
+
+            if(ai_ships_locations_alive.length == 0){
+              runGame(GameState.GAMEOVER);
+            }
+          }
+          else {
+            // Need to append image of miss sign to the event.target
+            console.log("MISS");
+          }
+
+          runGame(GameState.OPPONENTTURN);
+        }
+      });
       
       break;
     case GameState.OPPONENTTURN:
+      $("#game_title").text("Wait for opponent");
 
-      // Unselectable opponent grid
+      // Player can choose opponent grid
+      let t = 5;
+      $("#timer").html(t);
+      let t_interval = setInterval(function(){
+        t -= 1;
+        $("#timer").text(t);
 
+        if (t == 0){
+          clearInterval(t_interval);
+        }
+      }, 1000); 
 
-      // Fire Button gone
+      setTimeout(function(){
+        runGame(GameState.PLAYERTURN);
+      }, 3000); 
+
+      // AI needs to make a random selection from the selectable grid squares on the users side. 
+      // If the AI gets a hit, append a image over top the players ship
+      // If the AI gets a miss, append a image inside the grid the AI choose to shoot.
 
       console.log("opponent Turn")
-      // Opponent makes choice
       break;
   }
 }
@@ -288,7 +349,15 @@ function menu_button(){
   $("#actualGame").html(menu_html);
 }
 
-function handle_drop_patient(event, ui) {
+function make_ai_ship_choices() {
+  for(let i = 0; i < 8; ++i){
+    let randNum = Math.ceil(getRandomNumber(0, 24));
+    while(ai_ship_locations.includes(randNum) == true){
+      randNum = Math.ceil(getRandomNumber(0, 24));
+    }
+    ai_ship_locations.push(randNum);
+  }
 
+  ai_ships_locations_alive = ai_ship_locations;
 }
 
